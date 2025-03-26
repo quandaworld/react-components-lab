120d9495caee4791f36994358618ab04ea91dc4e
quandaworld <nhiquannguyen@gmail.com>
2025-03-25 22:21:35 -0700

    Fix bug: remove duplicated day element

--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -5,7 +5,6 @@ import WeatherIcon from '../WeatherIcon/WeatherIcon';
 const WeatherForecast = ({ day, img, imgAlt, conditions, time }) => {
   return (
     <div className='weather'>
-      <h2>{day}</h2>
       <WeatherData day={day} conditions={conditions} time={time} />
       <WeatherIcon img={img} imgAlt={imgAlt} />
     </div>
